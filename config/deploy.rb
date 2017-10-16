566643a211ed552784c799e3eea2b14548ae48d9
Anton Murygin <mibus32@gmail.com>
2017-10-16 22:44:09 +0300

    Secret key base works

--- config/deploy.rb
+++ config/deploy.rb
@@ -38,7 +38,7 @@ set :scm, :git
 
 # Default value for :linked_files is []
 #set :linked_files, %w{config/database.yml config/redis.yml}
-set :linked_files, %w{config/database.yml config/application.yml config/secrets.yml}
+set :linked_files, %w{config/database.yml config/application.yml}
 
 # Default value for linked_dirs is []
 set :linked_dirs, %w{log tmp/pids tmp/cache tmp/sockets vendor/bundle client/node_modules}
